c8762d6bd6086850803427c638d1162b67a36d8a
VictorCaique <vcds4747@gmail.com>
2022-05-13 20:57:02 -0300

    Mudanças Aula 13/05 (error)

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-import {ocultarSenha} from './model/components/segurança.js'
+import express from 'express';
+import PATH from 'path';
+import consign from 'consign';
+import bodyParser from 'body-parser'
+import ejs from 'ejs'
 
-console.log(ocultarSenha("Abacate"));
+import {ocultarSenha} from './model/components/segurança.js'
+var app = express();
+
+//npm install body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//nom install ejs
+app.engine('html', ejs);
+app.set('view engine', 'ejs');
+app.set('views', PATH.join(__dirname, '/view/'));
+
+
+consign().include('controller/routes').into(app);
+
+app.listen(8081, () => {
+    console.log("http://localhost:8081");
+})
+
+//console.log(ocultarSenha("Abacate"));
