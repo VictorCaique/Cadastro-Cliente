5937d8e40754afba3cd27353e156e7e26c494fd9
VictorCaique <vcds4747@gmail.com>
2022-05-20 22:25:52 -0300

    Controller CRUD pt2

--- controller/routes/usuario.js
+++ controller/routes/usuario.js
@@ -1,9 +1,80 @@
+import usuarioBD from "../../model/repositories/usuarioBD";
+import seguranca from "../../model/components/segurança";
+
 export default function usuario(app){
     app.get("/login", (req, res) => {
         if(req.query.fail){
             res.render('usuario/Login', {mensagemLogin: 'Usuário e/ou senha incorretos!'})
         }else{
             res.render('usuario/Login', {mensagemLogin: null})
+        }
+    })
+
+    app.get('/cadastro', (req, res) => {
+        if (req.query.fail){
+            res.render('usuario/Cadastro', {mensagem: 'Cadastro'});
+        } else {
+            res.render('usuario/Cadastro', {mensagem: null});
+        }
+    });
+
+    app.get('/lista/usuario', async (req, res, next) => {
+        try {
+            const docs = await usuarioBD.selectUsuario();
+            res.render('usuario/lista', { mensagem: 'lista de Usuários', docs });
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    app.get('/delete/usuario/:id', async (req, res, next) => {
+        try{
+            var id = req.params.id;
+            await usuarioBD.deleteUsuario(id);
+            const docs = await usuarioBD.selectUsuario();
+            res.render('usuario/Lista', {mensagem:'', usuario});
+            //JMeter
+        } catch (err) {
+            next(err);
+        }
+    });
+
+    app.get('/edit/usuario/:id', async (req, res, next) => {
+        try{
+            var id = req.params.id;
+            const usuario = await usuarioBD.getUsuarioId(id);
+            res.render('usuario/EditUsuario', {mensagem: '', usuario});
+        }catch (err) {
+            next(err);
+        }
+    })
+
+    app.post('/cadastro/usuario/edit/salvar', (req,res)=>{
+        var usuario = {
+            nome: req.body.nome,
+            senha: req.body.senha,
+            id: req.body.id
+        }
+
+        try{
+            usuarioBD.updateUsuario(usuario);
+            res.render('usuario/Sucesso', { mensagem: 'alterado'});
+        }catch (error) {
+            res.render('usuario/EditUsuario', { title: 'Edição Cadastro', mensagem: 'Erro no cadastrado' });
+        }
+    });
+
+    app.post('/cadastro/usuario/salvar', (req, res) => {
+        try{
+            var usuario = {
+                nome: req.body.nome,
+                senha: seguranca.ocultarsenha(req.body.senha)
+            }
+            usuarioBD.insertUsuario(usuario);
+            res.render('usuario/Sucesso', { mensagem: 'cadastrado' });
+        } catch (error) {
+            console.log(error);
+            res.render('usuario/Cadastro', { title: 'Cadastrado', mensagem: 'Erro no Cadastro'});
         }
     })
 
--- model/repositories/usuarioBD.js
+++ model/repositories/usuarioBD.js
@@ -1,13 +1,13 @@
 import usuarioBD from '../utils/db.js';
 import seguranca from '../components/segurança.js';
 
-async function selectUsuario(){
+export async function selectUsuario(){
     const conn = await usuarioBD.connect();
     const [rows] = await conn.query('SELECT * FROM usuario;');
     return rows;
 }
 
-async function getUsuarioId(id){
+export async function getUsuarioId(id){
     const conn = await usuarioBD.connect();
     const sql = 'SELECT * FROM usuario WHERE id = ?';
     const values = [id];
@@ -15,7 +15,7 @@ async function getUsuarioId(id){
     return rows[0] ? rows.length > 0 : null;
 }
 
-async function login(nome, senha){
+export async function login(nome, senha){
     const conn = await usuarioBD.connect();
     const sql = 'SELECT * FROM usuario WHERE nome=? and senha=?';
     const values = [nome, seguranca.ocultarsenha(senha)];
@@ -23,9 +23,22 @@ async function login(nome, senha){
     return rows ? rows.length > 0 : null;
 }
 
-async function insertUsuario(usuario){
+export async function insertUsuario(usuario){
     const conn = await usuarioBD.connect();
     const sql = 'INSERT INTO usuario (nome, senha) VALUES (?, ?);';
     const values = [usuario.nome, seguranca.ocultarsenha(usuario.senha)];
+    return await conn.query(sql, values);
+}
+
+export async function deleteUsuario(id){
+    const conn = await usuarioBD.connect();
+    const sql = 'DELETE FROM usuario WHERE id=?';
+    return await conn.query(sql, [id]);
+}
+
+export async function updateUsuario(usuario){
+    const conn = await usuarioBD.connect();
+    const sql = 'UPDATE usuario SET nome = ?, senha = ? WHERE id = ?;'
+    const values = [usuario.nome, seguranca.ocultarsenha(usuario.senha), usuario.id];
     return await conn.query(sql, values);
 }
