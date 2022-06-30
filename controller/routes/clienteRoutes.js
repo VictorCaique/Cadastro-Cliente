2530069258dd727fed4866c50c9a41a3c3f6f893
VictorCaique <vcds4747@gmail.com>
2022-06-30 14:58:59 -0300

    Fzendo bahianisse

--- controller/routes/clienteRoutes.js
+++ controller/routes/clienteRoutes.js
@@ -10,8 +10,8 @@ var Router = express.Router();
 Router.get('/', async (req, res, next) => {
     try {
         const docs = await usuarioBD.selectCliente();
-        res.render("usuario/lista", {
-          mensagem: "lista de Usuários",
+        res.render("cliente/index", {
+          mensagem: "",
           docs
         });
       } catch (err) {
@@ -19,6 +19,40 @@ Router.get('/', async (req, res, next) => {
       }
 })
 
-Router.get('/')
+Router.get('/cadastro', async (req, res) => {
+  if (req.query.fail) {
+    res.render("cliente/CadastroCliente", {
+      mensagem: "Cadastro"
+    });
+  } else {
+    res.render("cliente/CadastroCliente", {
+      mensagem: null
+    });
+  }
+})
+
+
+
+Router.post('/cadastro/save', async (req, res) => {
+  try {
+    await usuarioBD.insertCliente({
+      nome: req.body.nome,
+      idade: req.body.idade,
+      cpf: req.body.cpf,
+      endereco: req.body.endereco,
+    });
+    const docs = await usuarioBD.selectCliente();
+    res.render("cliente/index", {
+      mensagem: "Lista de Clientes",
+      docs
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("cliente/index", {
+      title: "Cadastrado",
+      mensagem: "Erro no Cadastro",
+    });
+  }
+})
 
 export default Router;
